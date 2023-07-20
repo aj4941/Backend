90fdd2fea80e63a2e89ddd92b3825408aff9ee3d
miiiinju1 <gms07073@ynu.ac.kr>
2023-07-20 14:37:10 +0900

    initial commit

---
+++ src/main/java/swm_nm/morandi/MorandiApplication.java
@@ -0,0 +1,13 @@
+package swm_nm.morandi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MorandiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MorandiApplication.class, args);
+    }
+
+}
