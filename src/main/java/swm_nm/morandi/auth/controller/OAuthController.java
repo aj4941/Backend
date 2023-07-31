abea5342d52c8d5d4d5e8910c978c38684958457
miiiinju1 <gms07073@ynu.ac.kr>
2023-07-31 14:23:19 +0900

    :art: [IMPROVE] Cookie에 Access Token 추가하도록 변경 및 백엔드 개발용 컨트롤러 생성 #54
    Cookie에 Access Token 추가하도록 변경 및 백엔드 개발용 컨트롤러 생성 #54

--- src/main/java/swm_nm/morandi/auth/controller/OAuthController.java
+++ src/main/java/swm_nm/morandi/auth/controller/OAuthController.java
@@ -2,9 +2,15 @@ package swm_nm.morandi.auth.controller;
 
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import swm_nm.morandi.auth.response.TokenDto;
 import swm_nm.morandi.auth.service.LoginService;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+import java.net.URI;
 
 
 @RestController
@@ -17,10 +23,24 @@ public class OAuthController {
 
     //Authorization Code도 모두 가져오는 경우에 사용하는 callback api
     @GetMapping("/{type}/callback")
-    public TokenDto googleLogin(@PathVariable String type, @RequestParam String code) {
+    public ResponseEntity<String> googleLogin(@PathVariable String type, @RequestParam String code, HttpServletResponse response) {
+        String accessToken = loginService.login(type, code).getAccessToken();
+
+        Cookie jwtCookie = new Cookie("accessToken", accessToken);
+        jwtCookie.setHttpOnly(true);
+        jwtCookie.setPath("/"); // Make it accessible for all paths
+        jwtCookie.setMaxAge(24 * 60 * 60); // Optional - set the cookie to expire after one day
+        jwtCookie.setSecure(true); // Optional - set secure flag for HTTPS
+
+        response.addCookie(jwtCookie);
+
+        return ResponseEntity.status(HttpStatus.FOUND).location(URI.create("http://localhost:3000/dashboard")).build();
+    }
+
+    //개발자 모드에서는 redirect를 하지 않고 access Token을 가져오는 api
+    @GetMapping("/{type}/callback/dev")
+    public TokenDto googleLoginforDevelop(@PathVariable String type, @RequestParam String code) {
         return loginService.login(type, code);
-
-
     }
 
     //만약 이미 access Toekn을 가지고 있는 경우에는 이 api를 사용한다.
