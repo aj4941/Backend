711d89b926c503674bc7355e98e68afce91c17c0
aj4941@naver.com <aj4941@naver.com>
2023-08-11 21:22:47 +0900

    [FIX] 사용자 테스트 레이팅 기록 api 오류 해결

--- src/main/java/swm_nm/morandi/test/dto/TestRatingDto.java
+++ src/main/java/swm_nm/morandi/test/dto/TestRatingDto.java
@@ -1,8 +1,13 @@
 package swm_nm.morandi.test.dto;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import lombok.*;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Getter @Setter
 @NoArgsConstructor
@@ -10,7 +15,9 @@ import java.time.LocalDate;
 @Builder
 public class TestRatingDto {
     private Long testId;
-    private LocalDate testDate;
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    private LocalDateTime testDate;
     private String testTypeName;
     private Long testRating;
 }
