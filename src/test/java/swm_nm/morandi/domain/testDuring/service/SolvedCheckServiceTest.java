6490ebee268ecc82f0d5fe1c76c454427345fb25
aj4941@naver.com <aj4941@naver.com>
2023-09-21 15:26:03 +0900

    :white_check_mark: [TEST] RunCodeServiceTest 테스트 작성

---
+++ src/test/java/swm_nm/morandi/domain/testDuring/service/SolvedCheckServiceTest.java
@@ -0,0 +1,13 @@
+package swm_nm.morandi.domain.testDuring.service;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class SolvedCheckServiceTest {
+
+
+    void
+
+}
---
+++ temp.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<ll, ll> pll;
+int dx[4] = { -1, 1, 0, 0 };
+int dy[4] = { 0, 0, -1, 1 };
+
+int main(void)
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    int a, b; cin >> a >> b;
+    cout << a + b;
+}
