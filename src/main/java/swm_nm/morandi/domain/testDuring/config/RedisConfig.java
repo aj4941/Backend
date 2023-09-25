5fc07697d5922ec18d7ff57c646b5afce97679db
miiiinju1 <gms07073@ynu.ac.kr>
2023-09-25 18:26:27 +0900

    :bug: [FIX] container 사이 redis Connection 문제 해결 #376
    close #376

--- src/main/java/swm_nm/morandi/domain/testDuring/config/RedisConfig.java
+++ src/main/java/swm_nm/morandi/domain/testDuring/config/RedisConfig.java
@@ -1,24 +1,25 @@
 package swm_nm.morandi.domain.testDuring.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
+
 @Configuration
+@RequiredArgsConstructor
 public class RedisConfig {
 
-    @Bean
-    public LettuceConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory();
-    }
+    private final RedisConnectionFactory redisConnectionFactory;
 
     @Bean
     public RedisTemplate<String, Object> redisTemplate() {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
-        template.setConnectionFactory(redisConnectionFactory());
+        template.setConnectionFactory(redisConnectionFactory);
         template.setKeySerializer(new StringRedisSerializer());
         template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
         return template;
