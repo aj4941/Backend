5dcd5781d8b50fe47d24c987e230ed7b4d075705
miiiinju1 <111269144+miiiinju1@users.noreply.github.com>
2023-10-11 14:08:35 +0900

    [HOTFIX] key설정 변경

--- src/main/java/swm_nm/morandi/domain/auth/constants/SecurityConstants.java
+++ src/main/java/swm_nm/morandi/domain/auth/constants/SecurityConstants.java
@@ -11,7 +11,7 @@ public class SecurityConstants {
     //임시로
     public static final long JWT_EXPIRATION = 600000000;
     public static final long REFRESH_TOKEN_EXPIRATION = 36000000;
-    public static final Key JWT_KEY = new SecretKeySpec("r5QyAI4s7GFJh4Nn4bUJHfZfQ5ks9ZlIwE4QwdZNCm5wefMpNkZq4T7GWM5szMZa".getBytes(), "HmacSHA512");//Keys.secretKeyFor(SignatureAlgorithm.HS512);
+    public static final Key JWT_KEY = Keys.secretKeyFor(SignatureAlgorithm.HS512);
 
     public static final Boolean FOR_DEVELOPER= true;
 
