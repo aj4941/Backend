2339175d74cab600851e262d391b65619389df60
miiiinju1 <gms07073@ynu.ac.kr>
2023-10-30 18:30:10 +0900

    :zap: [IMPROVE] 단순 조회 API Transaction readOnly 옵션 추가 #539
    close #539

--- src/main/java/swm_nm/morandi/domain/testInfo/service/TestTypeInfoService.java
+++ src/main/java/swm_nm/morandi/domain/testInfo/service/TestTypeInfoService.java
@@ -3,6 +3,7 @@ package swm_nm.morandi.domain.testInfo.service;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import swm_nm.morandi.domain.testInfo.dto.TestTypeDto;
 import swm_nm.morandi.domain.testInfo.entity.TestType;
 import swm_nm.morandi.domain.testInfo.mapper.TestTypeMapper;
@@ -16,6 +17,8 @@ import swm_nm.morandi.global.exception.errorcode.TestTypeErrorCode;
 public class TestTypeInfoService {
 
     private final TestTypeRepository testTypeRepository;
+
+    @Transactional(readOnly = true)
     public TestTypeDto getTestTypeDto(Long testTypeId) {
         TestType testType = testTypeRepository.findById(testTypeId).orElseThrow(() -> new MorandiException(TestTypeErrorCode.TEST_TYPE_NOT_FOUND));
         TestTypeDto testTypeDto = TestTypeMapper.convertToDto(testType);
