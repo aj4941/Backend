ed58157cd17dadbc211f6f7c0f8afe3239803161
aj4941@naver.com <aj4941@naver.com>
2023-11-01 18:15:56 +0900

    :pencil2: [FIX] 기존 연습문제가 존재하는 경우 기존 PK를 반환

--- src/main/java/swm_nm/morandi/domain/practice/service/PracticeProblemService.java
+++ src/main/java/swm_nm/morandi/domain/practice/service/PracticeProblemService.java
@@ -5,10 +5,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import swm_nm.morandi.domain.member.entity.Member;
 import swm_nm.morandi.domain.member.repository.MemberRepository;
-import swm_nm.morandi.domain.practice.repository.PracticeProblemRepository;
+import swm_nm.morandi.domain.practice.dto.PracticeIdDto;
 import swm_nm.morandi.domain.practice.dto.PracticeResultDto;
-import swm_nm.morandi.domain.practice.dto.PracticeStatus;
 import swm_nm.morandi.domain.practice.entity.PracticeProblem;
+import swm_nm.morandi.domain.practice.repository.PracticeProblemRepository;
 import swm_nm.morandi.domain.problem.entity.Problem;
 import swm_nm.morandi.domain.problem.repository.ProblemRepository;
 import swm_nm.morandi.global.exception.MorandiException;
@@ -17,9 +17,8 @@ import swm_nm.morandi.global.exception.errorcode.PracticeProblemErrorCode;
 import swm_nm.morandi.global.exception.errorcode.ProblemErrorCode;
 import swm_nm.morandi.global.utils.SecurityUtils;
 
-import java.time.LocalDateTime;
-
-import static swm_nm.morandi.domain.practice.dto.PracticeStatus.*;
+import java.time.LocalDate;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -32,27 +31,36 @@ public class PracticeProblemService {
 
     private final PracticeProblemRepository practiceProblemRepository;
 
-    public void savePraticeProblems(Long bojProblemId) {
+    public PracticeIdDto savePraticeProblems(Long bojProblemId) {
         Long memberId = SecurityUtils.getCurrentMemberId();
         Member member = memberRepository.findById(memberId).orElseThrow(
                 () -> new MorandiException(MemberErrorCode.MEMBER_NOT_FOUND));
+
+        Optional<PracticeProblem> optionalPracticeProblem = practiceProblemRepository.
+                findByMember_MemberIdAndProblem_BojProblemId(memberId, bojProblemId);
+
+        if (optionalPracticeProblem.isPresent()) {
+            PracticeProblem practiceProblem = optionalPracticeProblem.get();
+            return PracticeIdDto.getPracticeIdDto(practiceProblem.getPracticeProblemId());
+        }
+
         Problem problem = problemRepository.findProblemByBojProblemId(bojProblemId).orElseThrow(
                 () -> new MorandiException(ProblemErrorCode.PROBLEM_NOT_FOUND));
 
-        PracticeProblem practiceProblem = PracticeProblem.builder()
-                .practiceStatus(IN_PROGRESS)
-                .practiceDate(LocalDateTime.now())
+        PracticeProblem addPracticeProblem = PracticeProblem.builder()
+                .isSolved(false)
+                .practiceDate(LocalDate.now())
                 .problem(problem)
                 .member(member)
                 .build();
 
-        practiceProblemRepository.save(practiceProblem);
+        PracticeProblem savedPracticeProblem = practiceProblemRepository.save(addPracticeProblem);
+        return PracticeIdDto.getPracticeIdDto(savedPracticeProblem.getPracticeProblemId());
     }
 
     public PracticeResultDto practiceResult(Long practiceProblemId) {
         PracticeProblem practiceProblem = practiceProblemRepository.findById(practiceProblemId).orElseThrow(
                 () -> new MorandiException(PracticeProblemErrorCode.PRACTICE_PROBLEM_NOT_FOUND));
-
         PracticeResultDto practiceResultDto = PracticeResultDto.getPracticeResultDto(practiceProblem);
         return practiceResultDto;
     }
