3c450c94896b4646c21ff7473717589d819a29d7
miiiinju1 <gms07073@ynu.ac.kr>
2024-05-03 14:05:02 +0900

    :ambulance: 사소한 버그 수정

--- src/main/java/swm_nm/morandi/domain/codeSubmit/service/BaekjoonSubmitService.java
+++ src/main/java/swm_nm/morandi/domain/codeSubmit/service/BaekjoonSubmitService.java
@@ -49,7 +49,7 @@ import java.util.concurrent.TimeUnit;
 public class BaekjoonSubmitService {
 
     private final RestTemplate restTemplate = new RestTemplate();
-    private final String USER_AGENT = "Mozilla/5.0";
+    private final String USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36";
 
     private final RedisTemplate<String, Object> redisTemplate;
 
